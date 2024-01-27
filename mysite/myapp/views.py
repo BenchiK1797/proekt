ce7826ad730c97e51fffa693fbc6855e90246853
BenchiK1797 <FenYa_11@mail.ru>
2024-01-27 18:33:47 +0300

    не работает

---
+++ mysite/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+def index(request):
+    return render(request, 'myapp/index.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
